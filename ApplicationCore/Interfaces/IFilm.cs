e437fc32e252ca807c078582e7b825e082da0db8
JasserBOUKRYA <jasser.boukraya@esprit.tn>
2023-05-12 15:10:38 +0100

    first commit

---
+++ ApplicationCore/Interfaces/IFilm.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IFilm<T> where T : class
+    {
+        public Film GetFilm();
+    }
+}
